e9ab7e35b69131f1ae530fbe69b9de8131ff90f6
Hisham Alangadan <hishamal2002@gmail.com>
2024-08-07 21:32:23 +0530

    Added the kerala culture

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,19 +1,23 @@
 // import React from 'react';
 // import logo from '../assets/cetLogo.svg';
 import './styles/home.css';
+import keralaCulture from '../assets/kerala-stuff.png';
 
 const Home = () => {
   return (
     <div className="home">
       <div className="tint">
-      <div className="content">
-        <h1>ICCC2025</h1>
-        <h4>International Conference on <br/>Control, Communication and Computing</h4>
-        <p><i>Technical co-sponsor:</i> <br/> IEEE Kerala Section</p>
-        
-        <p>May 23 - 25, 2025</p>
-        {/* <a href="#" className="register-button">Register Now</a> */}
+        <div className="content">
+          <h1>ICCC2025</h1>
+          <h4>International Conference on <br/>Control, Communication and Computing</h4>
+          <p><i>Technical co-sponsor:</i> <br/> IEEE Kerala Section</p>
+          
+          <p>May 23 - 25, 2025</p>
+          {/* <a href="#" className="register-button">Register Now</a> */}
+        </div>
       </div>
+      <div className="kerala-stuff">
+        <img src={keralaCulture} alt="" />
       </div>
     </div>
   );
