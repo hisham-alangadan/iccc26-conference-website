e4b7ddba4eb836a317e9a826e82b09e6c1b87c32
Aravind Suresh Thakidayil <aravind.suresh@hotmail.com>
2024-08-08 21:52:08 +0530

    Tracks updated

--- src/components/tracks.tsx
+++ src/components/tracks.tsx
@@ -10,19 +10,22 @@ const TracksComponent = () => {
                     <table>
                         <header> Track 1 </header>
                         <ul>
-                            <li>Control Theory and Applications</li>
-                            <li>Intelligent Autonomous Systems</li>
                             <li>Architectures for Intelligent Control</li>
-                            <li>Networked Control Systems</li>
                             <li>Automotive Control Systems</li>
-                            <li>Industrial Process Control & Automation</li>
-                            <li>Manufacturing Systems Control</li>
+                            <li>Control of Aerospace Systems</li>
                             <li>Control of Constrained Systems</li>
-                            <li>Power System Operation and Control</li>
-                            <li>Control of Electric Drives</li>
-                            <li>Robotics and Aerospace Applications</li>
-                            <li>Renewable Energy and Smart Grid</li>
+                            <li>Control Theory and Applications</li>
+                            <li>Industrial Process Control & Automation</li>
+
+                            <li>Intelligent Autonomous Systems</li>
+                            <li>Manufacturing Systems Control</li>
                             <li>Navigation, Guidance and Control</li>
+
+                            <li>Networked Control Systems</li>
+                            <li>Power Electronics and Drives</li>
+                            <li>Power System Operation and Control</li>
+                            <li>Renewable Energy and Smart Grid</li>
+                            <li>Transportation Electrification</li>
                         </ul>
                     </table>
                 </div>
@@ -32,19 +35,21 @@ const TracksComponent = () => {
                             Track 2
                         </header>
                         <ul>
-                            <li>Wireless Communication and Sensor Networks</li>
-                            <li>RF and Microwave Systems</li>
-                            <li>Automation Robotics and Control</li>
-                            <li>Power Electronics and Instrumentation</li>
-                            <li>Signal Processing</li>
-                            <li>MEMS/NEMS</li>
-                            <li>Optoelectronics and Lightwave Systems</li>
-                            <li>VLSI and Embedded Systems</li>
-                            <li>Biomedical Engineering</li>
-                            <li>Reliability Engineering</li>
-                            <li>Network Systems and Protocols</li>
-                            <li>Nanoelectronics</li>
-                            <li>Computer Networks</li>
+                            <li> Acoustic and Audio Signal Processing </li>
+                            <li> Biomedical Signal Processing </li>
+                            <li> Instrumentation and Control </li>
+                            <li> MEMS/NEMS </li>
+                            <li> Nanoelectronics </li>
+                            <li> Network Systems and Protocols </li>
+                            <li> Optoelectronics and Lightwave Systems </li>
+                            <li> RF and Microwave Systems </li>
+                            <li> Reliability Engineering </li>
+                            <li> Robotics and Automation </li>
+                            <li> Signal Processing for Communications and Networking </li>
+                            <li> Two-dimensional Signal Processing and Video Analytics  </li>
+                            <li> VLSI and Embedded Systems </li>
+                            <li> Wireless Communication and Sensor Networks </li>
+
                         </ul>
                     </table>
                 </div>
@@ -54,19 +59,21 @@ const TracksComponent = () => {
                             Track 3
                         </header>
                         <ul>
-                            <li>Parallel and Distributed Computing</li>
-                            <li>Advanced Algorithms</li>
-                            <li>Computational Geometry and Computer Graphics</li>
-                            <li>Software Engineering and Formal Methods</li>
-                            <li>Data Mining and Information Retrieval</li>
-                            <li>Security and Privacy</li>
-                            <li>System Software Design and Implementation</li>
-                            <li>Natural Language Processing</li>
-                            <li>Block Chain Technologies</li>
-                            <li>Machine Learning</li>
-                            <li>Cloud Computing and IoT</li>
-                            <li>Digital Image Processing and Computer Vision</li>
-                            <li>Cyber Physical Systems</li>
+                            <li> Advanced Algorithms </li>
+                            <li> Big Data Analysis, Cloud Computing and IoT </li>
+                            <li> Blockchain Technologies </li>
+                            <li> Computational Geometry and Computer Graphics </li>
+                            <li> Computer Networks </li>
+                            <li> Cyber Physical Systems </li>
+                            <li> Data Mining and Information Retrieval </li>
+                            <li> Data Science, Artificial Intelligence and Machine Learning </li>
+                            <li> Digital Image Processing and Computer Vision </li>
+                            <li> Natural Language Processing </li>
+                            <li> Parallel and Distributed Computing </li>
+                            <li> Security and Privacy </li>
+                            <li> Software Engineering and Formal Methods </li>
+                            <li> System Software Design and Implementation </li>
+
                         </ul>
 
                     </table>
