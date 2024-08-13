bdaee403e8573dacc0a063f45670e150ee165b8c
Aravind Suresh Thakidayil <aravind.suresh@hotmail.com>
2024-08-13 09:50:17 +0530

    Minor changes

--- src/components/eventdetailscomponent.tsx
+++ src/components/eventdetailscomponent.tsx
@@ -1,9 +1,9 @@
 import './styles/eventdetailscomponent.css'
 
-export default function EventDetailsComponent () {
+export default function EventDetailsComponent() {
     return (
         <>
-        <h1 className='eventdetails-h1'>Coming soon...</h1>
+            <h1 className='eventdetails-h1'>Coming soon...</h1>
         </>
     )
 }
