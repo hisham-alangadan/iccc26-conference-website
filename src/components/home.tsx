c476d60ea45a4c01239be01f3870ae70f4cdb919
Srutav Tarun <srutavtarun@gmail.com>
2024-08-14 07:46:30 +0530

    ladder for timeline, fixed footer, updated reg details

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,19 +1,33 @@
-// import React from 'react';
-// import logo from '../assets/cetLogo.svg';
+import React, { useEffect } from 'react';
 import './styles/home.css';
-// import keralaCulture from '../assets/kerala-stuff.png';
+import bg1 from "../assets/cet.jpg";
+
+const images = [
+  bg1
+];
 
 const Home = () => {
+  // useEffect(() => {
+  //   let currentIndex = 0;
+  //   const slideshowInterval = setInterval(() => {
+  //     document.getElementById('slideshow').style.backgroundImage = `url(${images[currentIndex]})`;
+  //     currentIndex = (currentIndex + 1) % images.length;
+  //   }, 3000);
+
+  //   return () => clearInterval(slideshowInterval); // Cleanup on component unmount
+  // }, []);
+
   return (
     <div className="home">
-      <div className="tint">
-        <div className="content">
-          <h1>ICCC 2025</h1>
-          <h4>6<sup>th</sup> International Conference on <br/>Control, Communication and Computing</h4>
-          <p><i>Technical co-sponsor:</i> <br/> IEEE Kerala Section</p>
-          
-          <p>May 23 - 25, 2025</p>
-          {/* <a href="#" className="register-button">Register Now</a> */}
+      <div id="slideshow" className="slideshow">
+        <div className="tint">
+          <div className="content">
+            <h1>ICCC 2025</h1>
+            <h4>6<sup>th</sup> International Conference on <br/>Control, Communication and Computing</h4>
+            <p><i>Technical co-sponsor:</i> <br/> IEEE Kerala Section</p>
+            <p>May 23 - 25, 2025</p>
+            {/* <a href="#" className="register-button">Register Now</a> */}
+          </div>
         </div>
       </div>
       {/* <div className="kerala-stuff">
--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -10,7 +10,7 @@ export default function Timeline() {
                     <div className="title">1<sup>st</sup> Sep. 2024</div>
                     {/* <div className="descr">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic quibusdam fugiat est numquam harum, accusamus suscipit consequatur laboriosam!</div> */}
                 </li>
-                <li className='ulli'>
+                <li className='ulli ulli-2'>
                     <div className="date">Paper Submission Due Date</div>
                     <div className="title">30<sup>th</sup> Nov. 2024</div>
                     {/* <div className="descr">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos adipisci nobis nostrum vero nihil veniam.</div> */}
@@ -19,7 +19,7 @@ export default function Timeline() {
                     <div className="date">Notification of Acceptance</div>
                     <div className="title">15<sup>th</sup> Jan. 2025</div>
                 </li>
-                <li className='ulli'>
+                <li className='ulli ulli-2'>
                     <div className="date">Registration Starts</div>
                     <div className="title">15<sup>th</sup> Jan. 2025</div>
                 </li>
