02196f0203ca154e30b4293bceac98f43a495145
Srutav Tarun <srutavtarun@gmail.com>
2024-11-30 10:58:55 +0530

    made changes to text, font weight, titles, added a line below the timeline

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -7,27 +7,28 @@ export default function Timeline() {
             <ul className='timelinecss-ul'>
                 <li className='ulli'>
                     <div className="date">Paper Submission Opening Date</div>
-                    <div className="title">1<sup>st</sup> Sep. 2024</div>
+                    <div className="title black">1<sup>st</sup> Sep. 2024</div>
                     {/* <div className="descr">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas itaque hic quibusdam fugiat est numquam harum, accusamus suscipit consequatur laboriosam!</div> */}
                 </li>
                 <li className='ulli ulli-2'>
-                    <div className="date">Paper Submission Due Date</div>
-                    <div className="title black">31<sup>th</sup> Dec. 2024<br></br><div className='due-date-text'>(Rolling Reviews)</div></div>
+                    <div className="date">Paper Submission Deadline</div>
+                    <div className="title">31<sup>st</sup> Dec. 2024</div>
                     {/* <div className="descr">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos adipisci nobis nostrum vero nihil veniam.</div> */}
                 </li>
                 <li className='ulli'>
-                    <div className="date">Notification of Acceptance</div>
-                    <div className="title black">15<sup>th</sup> Feb. 2025</div>
+                    <div className="date">Notification to Authors</div>
+                    <div className="title black"><div className='due-date-text'>Rolling Reviews, Final Notification by</div>15<sup>th</sup> Feb. 2025</div>
                 </li>
                 <li className='ulli ulli-2'>
-                    <div className="date">Registration Starts</div>
+                    <div className="date">Registration Opens</div>
                     <div className="title black">15<sup>th</sup> Feb. 2025</div>
                 </li>
                 <li className='ulli'>
-                    <div className="date">Final Camera-ready Paper</div>
+                    <div className="date" id="crsd">Camera-Ready Submission Deadline</div>
                     <div className="title black">30<sup>th</sup> March 2025</div>
                 </li>
             </ul>
+            <div className='ist'> All deadlines are in Indian Standard Time (IST)</div>
         </div>
     )
 }
